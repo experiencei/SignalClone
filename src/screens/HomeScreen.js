90e74531b98e7f982e1f3e3570447f9ea856633c
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 09:35:33 +0100

    working on the homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
 import CustomListItem from '../components/CustomListItem'
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation}) => {
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+        headerBackTitle : "Signal",
+        headerStyle = {backgroundColor: "#fff"},
+        headerTitleStyle = {color: "black"},
+        headerTintColor : "black"
+    })
+  }, [input])
+
     return (
         <SafeAreaView>
             <ScrollView>
