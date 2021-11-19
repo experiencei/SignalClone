6ef032ac1e267530e79ce7017182ca8221168e2a
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 09:21:04 +0100

    working on the homescreen

--- src/components/CustomListItem.js
+++ src/components/CustomListItem.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-
+import { ListItem , Avatar } from 'react-native-elements'
 const CustomListItem = () => {
     return (
         <View>
