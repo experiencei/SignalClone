e2141c0daf8f1c73648582c5a9b2097c0bb6c78d
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 07:29:06 +0100

    adding react-navigation to the app

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -7,7 +7,12 @@ const LoginScreen = () => {
     return (
         <View>
              <StatusBar style="light" />
-           
+             <Image 
+               source={{
+                   uri : "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png"
+               }}
+              style={{width :200 , height : 200}}
+             />
         </View>
     )
 }
