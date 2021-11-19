e2fcb9802c3d510947cb15b49a0cec4cded25717
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 08:54:54 +0100

    working on the register screen

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,3 +1,7 @@
+import * as firebase from "firebase"
+import "firebase/auth";
+import "firebase/firestore";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDvdGevv8VSF6gUEgyGaMz4mrEPvH0xHAk",
     authDomain: "signal-9abae.firebaseapp.com",
@@ -6,3 +10,17 @@ const firebaseConfig = {
     messagingSenderId: "928488165032",
     appId: "1:928488165032:web:40b974c776d2493a2b7c9d"
   };
+
+
+let app
+
+if (firebase.apps.length === 0) {
+    app = firebase.initializeApp(firebaseConfig)
+} else {
+    app = firebase.app()
+}
+
+const db = app.firestore()
+const auth = firebase.auth()
+
+export { db, auth }
