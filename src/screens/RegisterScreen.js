71b520bd9f5b79fd9ac1ab45ffa58f6a06dec048
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 08:17:48 +0100

    working on the register screen

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Button , Input , Image } from 'react-native-elements';
 import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native';
 import { StatusBar } from "expo-status-bar"
 const RegisterScreen = ({ navigation}) => {
     return (
         <KeyboardAvoidingView>
             <StatusBar style="light" />
+            
+            <View style={styles.inputContainer}>
 
+            </View>
         </KeyboardAvoidingView>
     )
 }
