9d39322a56642f3869f5f764e4290e89a26ca759
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 08:02:58 +0100

    working on the register screen

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { StatusBar } from 'expo-status-bar';
+import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import RegisterScreen from './src/screens/RegisterScreen';
+import LoginScreen from './src/screens/LoginScreen';
+
+
+const Stack = createStackNavigator();
+const globalScreenOptions =  {
+  headerTintColor: '#FFF',
+  headerTitleStyle : { color : "white"},
+  headerStyle: { backgroundColor: '#2C6BED' }
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+           <Stack.Screen name='Login' component={LoginScreen} />
+            <Stack.Screen name='Register' component={RegisterScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
