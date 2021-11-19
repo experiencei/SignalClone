076f942e8d30baebac123a2696cc892857ba2b3c
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 10:11:29 +0100

    working on the homescreen

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import RegisterScreen from './src/screens/RegisterScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen';
+import AddChatScreen from './src/screens/AddChatScreen';
 
 
 const Stack = createStackNavigator();
@@ -22,6 +23,7 @@ export default function App() {
            <Stack.Screen name='Login' component={LoginScreen} />
             <Stack.Screen name='Register' component={RegisterScreen} />
             <Stack.Screen name='Home' component={HomeScreen} />
+            <Stack.Screen name='AddChat' component={AddChatScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
