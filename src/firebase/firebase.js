b3e8a775746b63e9dc4f503722217d46c4bbb16e
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 07:06:30 +0100

    adding react-navigation to the app

---
+++ src/firebase/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDvdGevv8VSF6gUEgyGaMz4mrEPvH0xHAk",
+    authDomain: "signal-9abae.firebaseapp.com",
+    projectId: "signal-9abae",
+    storageBucket: "signal-9abae.appspot.com",
+    messagingSenderId: "928488165032",
+    appId: "1:928488165032:web:40b974c776d2493a2b7c9d"
+  };
