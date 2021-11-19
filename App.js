5ca21f4ae306f360288de5e07e1e7bf71de73935
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 09:02:21 +0100

    working on the homescreen

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import RegisterScreen from './src/screens/RegisterScreen';
 import LoginScreen from './src/screens/LoginScreen';
+import HomeScreen from './src/screens/HomeScreen';
 
 
 const Stack = createStackNavigator();
