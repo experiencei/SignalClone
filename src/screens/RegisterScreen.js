957c7d1decfd635ebb118acc22ae01a0f9cad028
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 08:28:43 +0100

    working on the register screen

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button , Input , Image } from 'react-native-elements';
 import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native';
 import { StatusBar } from "expo-status-bar"
 const RegisterScreen = ({ navigation}) => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
+
+    const register = () => {
+        
+    }
+
     return (
         <KeyboardAvoidingView>
             <StatusBar style="light" />
@@ -12,6 +21,31 @@ const RegisterScreen = ({ navigation}) => {
             <View style={styles.inputContainer}>
                 <Input 
                     placeholder="Full Name"
+                    autoFocus
+                    type="text"
+                    value={name}
+                    onChangeText={(text) => setName(text)}
+                />
+                <Input 
+                    placeholder="Eamil"
+                    type="email"
+                    value={email}
+                    onChangeText={(text) => setEmail(text)}
+                />
+                <Input 
+                    placeholder="Password"
+                    secureTextEntry
+                    type="password"
+                    value={password}
+                    onChangeText={(text) => setPassword(text)}
+                />
+                <Input 
+                    placeholder="Profile Picture URL (optional)"
+                    autoFocus
+                    type="text"
+                    value={imageUrl}
+                    onChangeText={(text) => setImageUrl(text)}
+                    onSubmitEditing={register}
                 />
             </View>
         </KeyboardAvoidingView>
