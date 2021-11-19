d9e704dd1dc6e1c194ca8e4a10bbaad07beeb004
experiencei <ayelojahighbee01@gmail.com>
2021-11-19 10:25:25 +0100

    working on the homescreen

--- src/screens/AddChatScreen.js
+++ src/screens/AddChatScreen.js
@@ -1,14 +1,45 @@
-import React from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Button , Input , Image } from 'react-native-elements';
+import { db } from '../firebase/firebase';
 
-const AddChatScreen = () => {
+
+const AddChatScreen = ({ navigation }) => {
+  const [input , setInput] = useState('');
+
+   useLayoutEffect(() => {
+    navigation.setOptions({
+        title="Add a new Chat",
+        headerBackTitle : "Chats",
+         })
+   }, [navigation]);
+
+   const createChat = async () => {
+       await db.collection("chats").add({
+           chatName : input
+       }).then(() => {
+           navigation.goBack()
+       }).catch((error) => {alert(error)})
+   }
+
     return (
-        <View>
-            <Text></Text>
+        <View style={styles.container}>
+            <Input 
+                placeholder="Enter a chat name"
+                value={input}
+                onChangeText={(text) => setInput(text)}
+                onSubmitEditing={createChat}
+                leftIcon={
+                    <Icon name="wechat" type="antdesign" size={24} color="black"/>
+                }
+            />
+            <Button onPress={createChat} title="Create a new chat"/>
         </View>
     )
 }
 
 export default AddChatScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {}
+})
